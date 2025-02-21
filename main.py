0cf52df9fb3158f8d077ea346c7ad1ba17c4d951
ChaseWink05 <155601183+ChaseWink05@users.noreply.github.com>
2025-02-20 21:14:15 -0500

    updating the import statement

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import streamlit as st
-#from streamlit-extras.let_it_rain import rain
+from streamlit_extras.let_it_rain import rain
 from streamlit_calendar import calendar
 import Dashboard as DB
 import ToDoList as TDL
